eaaa9fc237cb9a828bb4f6037581b1be2b2dec4b
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-02-20 15:52:36 +0900

    C++ AI 폴더 생성 및 BTTask 테스트

---
+++ Source/CCD/Public/AI/BTTask_Test.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/BehaviorTree/BTTaskNode.h"
+#include "BTTask_Test.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CCD_API UBTTask_Test : public UBTTaskNode
+{
+	GENERATED_BODY()
+};
