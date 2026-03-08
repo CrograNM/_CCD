2fa90aa3bedf9fda584f13e0bf5ab87a88da2275
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-03-08 20:51:02 +0900

    시야판정 위젯 - 1
    머티리얼 및 애니메이션 제작까지 완료

---
+++ Source/CCD/Public/Widget/EyeWidget.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "EyeWidget.generated.h"
+
+UCLASS()
+class CCD_API UEyeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
