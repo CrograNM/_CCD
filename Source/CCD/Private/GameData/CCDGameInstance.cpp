9b37cd2a467dc5c907d43ce97417130b05ea75e9
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-03-20 22:36:39 +0900

    Game Data 시스템 구축 - 2
    메인 메뉴에서 유저 이름을 설정하고 저장할 수 있는 기능 추가

--- Source/CCD/Private/GameData/CCDGameInstance.cpp
+++ Source/CCD/Private/GameData/CCDGameInstance.cpp
@@ -1,2 +1,32 @@
 
 #include "GameData/CCDGameInstance.h"
+#include "GameData/CCDSaveGame.h"
+#include "Kismet/GameplayStatics.h"
+
+void UCCDGameInstance::Init()
+{
+	Super::Init();
+	UserProfileName = GetSavedName();
+}
+
+void UCCDGameInstance::SaveCustomName(FString NewName)
+{
+	UserProfileName = NewName;
+	if (UCCDSaveGame* SaveInstance = Cast<UCCDSaveGame>(UGameplayStatics::CreateSaveGameObject(UCCDSaveGame::StaticClass())))
+	{
+		SaveInstance->SavedPlayerName = NewName;
+		UGameplayStatics::SaveGameToSlot(SaveInstance, SaveSlotName, 0);
+	}
+}
+
+FString UCCDGameInstance::GetSavedName() const
+{
+	if (UGameplayStatics::DoesSaveGameExist(SaveSlotName, 0))
+	{
+		if (UCCDSaveGame* LoadedGame = Cast<UCCDSaveGame>(UGameplayStatics::LoadGameFromSlot(SaveSlotName, 0)))
+		{
+			return LoadedGame->SavedPlayerName;
+		}
+	}
+	return TEXT("None");
+}
--- Source/CCD/Public/GameData/CCDGameInstance.h
+++ Source/CCD/Public/GameData/CCDGameInstance.h
@@ -9,4 +9,17 @@ UCLASS()
 class CCD_API UCCDGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void Init() override;
+
+	UFUNCTION(BlueprintCallable)
+	void SaveCustomName(FString NewName);
+
+	UFUNCTION(BlueprintCallable)
+	FString GetSavedName() const;
+
+private:
+	FString UserProfileName;
+	FString SaveSlotName = TEXT("UserProfile");
 };
