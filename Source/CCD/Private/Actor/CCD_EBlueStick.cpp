df7706a43fa4cc8d5b720497837b722d9a71d3be
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-04-14 01:30:17 +0900

    블루스틱 : 1인 플레이 기준에서 온오프 성공적
    - 동기화 진행시 로직을 뜯어고칠 필요 존재함 (다른 플레이어에게도 블루스틱 장비가 존재하므로)

--- Source/CCD/Private/Actor/CCD_EBlueStick.cpp
+++ Source/CCD/Private/Actor/CCD_EBlueStick.cpp
@@ -5,6 +5,7 @@
 #include "Components/SphereComponent.h"
 #include "Components/PointLightComponent.h"
 #include "Kismet/KismetMaterialLibrary.h"
+#include "Net/UnrealNetwork.h"
 
 
 ACCD_EBlueStick::ACCD_EBlueStick()
@@ -26,9 +27,79 @@ ACCD_EBlueStick::ACCD_EBlueStick()
 	DeviceLight->Intensity = 5000.f;
 }
 
+void ACCD_EBlueStick::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ACCD_EBlueStick, bIsLEDOn); // 변수 동기화 등록
+}
+
+void ACCD_EBlueStick::ExecuteAction()
+{
+	if (!HasAuthority()) return;
+	bIsLEDOn = !bIsLEDOn; // LED 상태 토글
+	UpdateUVLightEffect();
+}
+
+void ACCD_EBlueStick::OnEquipped()
+{
+	if (HasAuthority())
+	{
+		bIsLEDOn = true; // 장착 시 LED 켜기
+		UpdateUVLightEffect();
+	}
+}
+
+void ACCD_EBlueStick::OnUnequipped()
+{
+	if (HasAuthority())
+	{
+		bIsLEDOn = false; // 해제 시 LED 끄기
+		UpdateUVLightEffect();
+	}
+}
+
 void ACCD_EBlueStick::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	// 머티리얼 인스턴스 생성 (블루프린트에서 StaticMesh에 할당된 재질 인덱스 0번 가정)
+	if (MeshComp)
+	{
+		MeshLightMID = MeshComp->CreateAndSetMaterialInstanceDynamic(0);
+		UpdateMeshLightMID();
+	}
+}
+
+void ACCD_EBlueStick::OnRep_IsLEDOn()
+{
+}
+
+void ACCD_EBlueStick::UpdateUVLightEffect()
+{
+	// 1. 장비 자체 라이트 제어
+	if (DeviceLight)
+	{
+		DeviceLight->SetVisibility(bIsLEDOn);
+	}
+
+	// 2. MPC를 통한 전역 데칼 효과 제어
+	if (UVLightMPC)
+	{
+		float Intensity = bIsLEDOn ? 1.0f : 0.0f;
+		UKismetMaterialLibrary::SetScalarParameterValue(GetWorld(), UVLightMPC, FName("UV_Global_Intensity"), Intensity);
+	}
+	
+	// 3. 메쉬 머티리얼 인스턴스 업데이트 (자체 발광 효과)
+	UpdateMeshLightMID();
+}
+
+void ACCD_EBlueStick::UpdateMeshLightMID()
+{
+	if (MeshLightMID)
+	{
+		FLinearColor EmissiveColor = bIsLEDOn ? FLinearColor(0.0f, 5.0f, 10.0f, 1.0f) :FLinearColor(0.12f, 0.16f, 0.21f, 1.0f);
+		MeshLightMID->SetVectorParameterValue(FName("EmissiveColor"), EmissiveColor);
+	}
 }
 
 void ACCD_EBlueStick::Tick(float DeltaTime)
@@ -38,13 +109,9 @@ void ACCD_EBlueStick::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 	
 	// 자신의 위치와 감지 반경을 MPC에 업데이트 (전역적으로 데칼들이 참조함)
-	if (UVLightMPC)
+	if (UVLightMPC && bIsLEDOn)
 	{
 		UKismetMaterialLibrary::SetVectorParameterValue(GetWorld(), UVLightMPC, FName("LampPosition"), FLinearColor(GetActorLocation()));
 		UKismetMaterialLibrary::SetScalarParameterValue(GetWorld(), UVLightMPC, FName("LampRadius"), DetectionSphere->GetUnscaledSphereRadius());
-	}
-	else 
-	{
-		UE_LOG(LogTemp, Warning, TEXT("[EBlueStick] UVLightMPC is not assigned!"));
 	}
 }
--- Source/CCD/Public/Actor/CCD_EBlueStick.h
+++ Source/CCD/Public/Actor/CCD_EBlueStick.h
@@ -16,10 +16,14 @@ class CCD_API ACCD_EBlueStick : public ACCD_EquipActor_Base
 public:
 	ACCD_EBlueStick();
 	virtual void Tick(float DeltaTime) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 
+	virtual void ExecuteAction() override;
+	virtual void OnEquipped() override;
+	virtual void OnUnequipped() override;
+	
 protected:
 	virtual void BeginPlay() override;
-
 	// 장비 외형
 	UPROPERTY(VisibleAnywhere, Category = "Equipment")
 	TObjectPtr<UStaticMeshComponent> MeshComp;
@@ -35,11 +39,16 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Detection")
 	class UMaterialParameterCollection* UVLightMPC;
 	
-	//UPROPERTY(EditAnywhere, Category = "Design | Sound")
-	//TObjectPtr<USoundBase> OnSound; 
+	UPROPERTY(ReplicatedUsing = OnRep_IsLEDOn)
+	bool bIsLEDOn = false;
+	UFUNCTION()
+	void OnRep_IsLEDOn();
 	
-	//UPROPERTY(EditAnywhere, Category = "Design | Sound")
-	//TObjectPtr<USoundBase> OffSound;
+	void UpdateUVLightEffect();
+	
+	UPROPERTY()
+	UMaterialInstanceDynamic* MeshLightMID; // 메쉬 전용 비주얼 업데이트 머티리얼 인스턴스
+	void UpdateMeshLightMID();
 	
 private:
 	const int32 DetectionStencilValue = 2; // 감지 시 적용할 스텐실 값 (포스트 프로세스에서 2번을 푸른 빛으로 설정 가정)
