df5aa0ea63e005fb61c13e18002abc3ecfe5c946
jimw0201 <jimw0201@gmail.com>
2026-04-20 03:05:40 +0900

    NavMeshBoundsVolume 문제 해결

--- Source/CCD/Private/AI/CCD_173_AIController.cpp
+++ Source/CCD/Private/AI/CCD_173_AIController.cpp
@@ -3,12 +3,21 @@
 
 #include "AI/CCD_173_AIController.h"
 #include "BehaviorTree/BehaviorTree.h"
-#include "BehaviorTree/BlackboardComponent.h"
-#include "Kismet/GameplayStatics.h"
+#include "Navigation/PathFollowingComponent.h"
+
+ACCD_173_AIController::ACCD_173_AIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UCCDPathFollowingComponent>(TEXT("PathFollowingComponent")))
+{
+}
 
 void ACCD_173_AIController::OnPossess(APawn* InPawn)
 {
 	Super::OnPossess(InPawn);
+	
+	if (UCCDPathFollowingComponent* PathFollower = Cast<UCCDPathFollowingComponent>(GetPathFollowingComponent()))
+	{
+		PathFollower->SetAcceptanceRadius(80.f); 
+	}
 
 	if (BTAsset)
 	{
--- Source/CCD/Public/AI/CCD_173_AIController.h
+++ Source/CCD/Public/AI/CCD_173_AIController.h
@@ -3,19 +3,35 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "AIController.h"
+#include "DetourCrowdAIController.h"
+#include "Navigation/CrowdFollowingComponent.h"
 #include "CCD_173_AIController.generated.h"
 
 /**
  * 
  */
+
 UCLASS()
-class CCD_API ACCD_173_AIController : public AAIController
+class CCD_API UCCDPathFollowingComponent : public UCrowdFollowingComponent
+{
+	GENERATED_BODY()
+public:
+	UCCDPathFollowingComponent()
+	{
+		bReachTestIncludesAgentRadius = true;
+		bReachTestIncludesGoalRadius = true;
+	}
+};
+
+UCLASS()
+class CCD_API ACCD_173_AIController : public ADetourCrowdAIController
 {
 	GENERATED_BODY()
 	
 public:
 	virtual void OnPossess(APawn* InPawn) override;
+	
+	ACCD_173_AIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
 protected:
 	// 에디터에서 사용할 비헤이비어 트리 에셋을 선택
