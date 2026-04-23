38ff38421e65af0de959923a575753758e9d0e97
sungwookcho@LAPTOP <fdsa7563@gmail.com>
2026-04-23 23:51:27 +0900

    TAB : net mode, map 이름 가져오게 수정

--- Source/CCD/Private/Widget/TabletWidget.cpp
+++ Source/CCD/Private/Widget/TabletWidget.cpp
@@ -1,2 +1,40 @@
 
 #include "Widget/TabletWidget.h"
+
+#include "GameFramework/GameStateBase.h"
+#include "Kismet/GameplayStatics.h"
+
+FString UTabletWidget::GetCurrentCleanMapName() const
+{
+	// '/Game/Maps/Level1' 대신 'Level1'만 가져옴
+	FString MapName = UGameplayStatics::GetCurrentLevelName(GetWorld(), true);
+	return MapName;
+}
+
+FString UTabletWidget::GetGameNetModeText() const
+{
+	if (UWorld* World = GetWorld())
+	{
+		// 월드의 NetMode를 확인합니다.
+		ENetMode NetMode = World->GetNetMode();
+        
+		// NM_Standalone: 완전한 싱글 플레이
+		// NM_ListenServer: 방장으로서 멀티 플레이 중
+		// NM_Client: 게스트로서 멀티 플레이 중
+		if (NetMode == NM_Standalone)
+		{
+			return TEXT("SINGLE");
+		}
+		else
+		{
+			// 접속자 수까지 표시
+			if (AGameStateBase* GS = World->GetGameState())
+			{
+				int32 Count = GS->PlayerArray.Num();
+				return FString::Printf(TEXT("MULTI (%d Players)"), Count);
+			}
+			return TEXT("MULTI");
+		}
+	}
+	return TEXT("UNKNOWN");
+}
--- Source/CCD/Public/Widget/TabletWidget.h
+++ Source/CCD/Public/Widget/TabletWidget.h
@@ -13,4 +13,12 @@ class CCD_API UTabletWidget : public UUserWidget
 public:
 	UFUNCTION(BlueprintImplementableEvent, Category = "Tablet")
 	void UpdateTablet();
+	
+	// 현재 맵 이름 반환 (경로 제외, 확장자 제외)
+	UFUNCTION(BlueprintCallable, Category = "GameInfo")
+	FString GetCurrentCleanMapName() const;
+
+	// 싱글, 멀티 모드 구분 텍스트 반환
+	UFUNCTION(BlueprintCallable, Category = "GameInfo")
+	FString GetGameNetModeText() const;
 };
